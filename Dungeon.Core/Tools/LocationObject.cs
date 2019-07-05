9e884dbbf6a3a5496b29c0be91a531184675a4d0
Kristina <52497296+Beneditova@users.noreply.github.com>
2019-07-05 10:02:48 +0300

    Add files via upload

---
+++ Dungeon.Core/Tools/LocationObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Core
+{
+    public enum LocationObject
+    {
+        Path = 0,
+        Player = 1,
+        Terrain = 3,
+        Enemy = 8
+    }
+}
