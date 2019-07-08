c6da5febdc2b1c13c39318f4e89405adcce17773
Kristina <52497296+Beneditova@users.noreply.github.com>
2019-07-08 20:28:04 +0300

    Add files via upload

---
+++ Dungeon.Core/Location/LocationCollector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Core
+{
+    public class LocationCollector
+    {
+        public List<LocationObject[,]> location = new List<LocationObject[,]>
+        {
+            new LocationObject[,]
+            {
+                 { LocationObject.Player, LocationObject.Path, LocationObject.Enemy, LocationObject.Path },
+                  { LocationObject.Terrain, LocationObject.Terrain, LocationObject.Path, LocationObject.Path },
+                  { LocationObject.Path, LocationObject.Path, LocationObject.Path, LocationObject.Path},
+            },
+
+            new LocationObject[,]
+            {
+                  { LocationObject.Player, LocationObject.Path, LocationObject.Path, LocationObject.Terrain },
+                  { LocationObject.Terrain, LocationObject.Enemy, LocationObject.Terrain, LocationObject.Path },
+                  { LocationObject.Path, LocationObject.Path, LocationObject.Path, LocationObject.Path},
+            }
+        };
+
+
+        
+       
+    }
+}
+
