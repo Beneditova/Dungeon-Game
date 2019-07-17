248283bb7148d29aea00d711d9fafacda719a94c
Kristina <52497296+Beneditova@users.noreply.github.com>
2019-07-17 13:31:37 +0300

    Fight class added for the combat

---
+++ Dungeon.Core/Engine/Fight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Core
+{
+    public class Fight
+    {
+        private IFightNotifiations _fightNotifiations;
+
+        public Fight(IFightNotifiations fightNotifiations)
+        {
+            _fightNotifiations = fightNotifiations;
+        }
+
+        public void FightHappend()
+        {
+            var hero = new Warrior(100, 15, 20);
+            var enemy = new Monk(100, 7, 30);
+
+            var engine = new GameEngine(hero, enemy, _fightNotifiations);
+            Console.WriteLine("The fierce battle begins!");
+          
+            engine.Fight();
+
+            Console.WriteLine("Winner is: " + engine.Winner);
+        }
+    }
+}
