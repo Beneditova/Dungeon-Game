e3f132da0d722a4f005c408901a5499396de651e
Kristina <52497296+Beneditova@users.noreply.github.com>
2019-07-29 17:09:03 +0300

    ChooseGatherer added

---
+++ Dungeon.Core/Tools/ChooseGatherer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Core
+{
+    public enum ChooseGatherer
+    {
+        Mage = 1,
+        Monk = 2,
+        Rogue = 3,
+        Warrior = 4,
+    }
+}
