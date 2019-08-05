0bb08e2c15e38719846d225a13938fea8f42a240
Kristina <52497296+Beneditova@users.noreply.github.com>
2019-08-05 10:59:52 +0300

    new classes

---
+++ Dungeon.Core/EnemyFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Core
+{
+    public class EnemyFactory
+    {
+        public List<BaseGatherer> Enemy = new List<BaseGatherer>
+        {
+            new Monk(50,12,15),
+            new Rogue(50,10,20),
+            new Mage(50,5,25),
+       };
+    }
+}
